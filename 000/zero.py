ddce26003951a8092d6bdb086d06ac0076b18d8d
Kaiyuan Liu <jungle.chuan@gmail.com>
2015-02-26 11:00:36 +0000

    add English explanation for each test

--- 000/zero.py
+++ 000/zero.py
@@ -1,5 +1,7 @@
 """
-将你的 QQ 头像（或者微博头像）右上角加上红色的数字，类似于微信未读信息数量那种提示效果。 类似于图中效果
+将你的 QQ 头像（或者微博头像）右上角加上红色的数字，类似于微信未读信息数量那种提示效果。
+
+To add red color text to the right-top of a image
 """
 
 import os, sys
