d2da00e2fc80efad67380ff360b2aab08f3abb65
Kaiyuan Liu <jungle.chuan@gmail.com>
2015-03-07 10:13:29 +0000

    init 008, have not finished yet

---
+++ 008/eight.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+"""
+一个HTML文件，找出里面的正文.
+"""
+
+import re
+
+class SpiderHTML(object):
+
+    def __init__(self):
+        pass
+
+    def search_body(object):
+        pass
