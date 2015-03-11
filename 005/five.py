593c54472abc10c38c898c2d59e484e727f92c42
Kaiyuan Liu <jungle.chuan@gmail.com>
2015-03-11 14:25:47 +0000

    modify 005 comments and add some parameters to function make it reuseable

--- 005/five.py
+++ 005/five.py
@@ -7,46 +7,45 @@ resizing all the images from a directory, which makes the resolution of images i
 import os
 from PIL import Image
 
-IPHONE5_IMAGE_WIDTH = 1136  # iPhone5 width
-IPHONE5_IMAGE_HEIGHT = 640  # iPhone5 height
+IPHONE_IMAGE_WIDTH = 1136  # iPhone5 width
+IPHONE_IMAGE_HEIGHT = 640  # iPhone5 height
 IMAGES_TYPE = ['*.jpg', '*.jpeg', '*png', '*.tif', '*.tiff']  # image type allowed
 
 
 class ResizeImage(object):
 
-    """
-    initialize parameters
-    @param org_image: orginal image path
-    """
     def __init__(self, org_image=None):
+        """
+        initialize parameters
+        @param org_image: orginal image path
+        """
         self._org_image = org_image
         self._img = None
 
-    """
-    open image using PIL
-    @param org_image: orginal image path
-    @return: self for chain purpose
-    """
     def open(self, org_image):
+        """
+        open image using PIL
+        @param org_image: orginal image path
+        @return: self for chain purpose
+        """
         if os.path.exists(org_image):
             self._org_image = org_image
             self._img = Image.open(self._org_image)
         return self
 
-    """
-    resize image
-    @param dst_image: the destination image path
-    @param dst_width: the destination image width default is iPhone5 screen width
-    @param dst_height: the destination image height default is iPhone5 screen height
-    @param quality: the destination image quality after resizing, default is 75
-    @return: resized image
-    """
     def resize_image(self, dst_image=None,
-                     dst_width=IPHONE5_IMAGE_WIDTH,
-                     dst_height=IPHONE5_IMAGE_HEIGHT,
+                     dst_width=IPHONE_IMAGE_WIDTH,
+                     dst_height=IPHONE_IMAGE_HEIGHT,
                      quality=75):
-
-        org_image_name, org_image_type = os.path.splitext(self._org_image)
+        """
+        resize image
+        @param dst_image: the destination image path
+        @param dst_width: the destination image width default is iPhone6 screen width
+        @param dst_height: the destination image height default is iPhone6 screen height
+        @param quality: the destination image quality after resizing, default is 75
+        @return: resized image
+        """
+        org_image_name,  org_image_type = os.path.splitext(self._org_image)
         org_width, org_height = self._img.size
         width_ratio = None
         height_ratio = None
@@ -69,34 +68,52 @@ class ResizeImage(object):
             extra_text = dst_image if dst_image else 'resized'
             dst_image = org_image_name+"_"+extra_text+org_image_type
             return self._img.resize(
-                                    (int(org_width*ratio), int(org_height*ratio)),
-                                    Image.ANTIALIAS).save(dst_image, quality=quality)
+                (int(org_width*ratio), int(org_height*ratio)),
+                Image.ANTIALIAS).save(dst_image, quality=quality)
+        self._img = None
+        self._org_image = None
 
-    """
-    invoke resizing function according to the method
-    @param method: which method is used to resize images, default is glob
-    """
-    def resize_all_images(self, method="glob", path=os.getcwd()):
+    def resize_all_images(self,
+                          method="glob",
+                          path=os.getcwd(),
+                          dst_image="iPhone6_resized",
+                          dst_width=IPHONE_IMAGE_WIDTH,
+                          dst_height=IPHONE_IMAGE_HEIGHT):
+        """
+        invoke resizing function according to the method
+        @param method: which method is used to resize images, default is glob
+        @param path: the path of directory, default is the current directory
+        @param dst_image: the destination image path
+        @param dst_width: the destination image width default is iPhone6 screen width
+        @param dst_height: the destination image height default is iPhone6 screen height
+        @param
+        """
         result = getattr(self, "_resize_all_images_by_"+method, None)
         if result is not None:
-            result(path)
+            result(path, dst_image, dst_width, dst_height)
 
-    """
-    resize all the images from some directory by using glob method
-    @param path: the path of directory, default is the current directory
-    """
-    def _resize_all_images_by_glob(self, path):
+    def _resize_all_images_by_glob(self, path, dst_image, dst_width, dst_height):
+        """
+        resize all the images from some directory by using glob method
+        @param path: the path of directory, default is the current directory
+        @param dst_image: the destination image path
+        @param dst_width: the destination image width
+        @param dst_height: the destination image height
+        """
         import glob
         images_list = [image for image_type in IMAGES_TYPE
                        for image in glob.glob(os.path.abspath(os.path.join(path, image_type)))]
         for one_image in images_list:
-            self.open(one_image).resize_image()
+            self.open(one_image).resize_image(dst_image=dst_image, dst_width=dst_width, dst_height=dst_height)
 
-    """
-    resize all the images from some directory by using os.walk and fnmatch
-    @param path: the path of directory, default is the current directory
-    """
-    def _resize_all_images_by_oswalk(self, path):
+    def _resize_all_images_by_oswalk(self, path, dst_image, dst_width, dst_height):
+        """
+        resize all the images from some directory by using os.walk and fnmatch
+        @param path: the path of directory, default is the current directory
+        @param dst_image: the destination image path
+        @param dst_width: the destination image width
+        @param dst_height: the destination image height
+        """
         import fnmatch
         images_list = [os.path.abspath(os.path.join(root, file_name).replace('\\', '/'))
                        for root, subdir, filenames in os.walk(path)
@@ -104,7 +121,7 @@ class ResizeImage(object):
                        for file_name in fnmatch.filter(filenames, image_type)]
 
         for one_image in images_list:
-            self.open(one_image).resize_image()
+            self.open(one_image).resize_image(dst_image=dst_image, dst_width=dst_width, dst_height=dst_height)
 
 
 if __name__ == "__main__":
